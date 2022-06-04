75d0ffd73a1721a37c8867f9a80339341cc70690
FilipSzweda <filip.szweda1@gmail.com>
2022-06-04 21:08:16 +0200

    I hate this

--- main.py
+++ main.py
@@ -1,30 +1,84 @@
-import pandas
 from matplotlib import pyplot
+import numpy as np
+import pandas
 
 
-def lagrange(step, data):
-    points = data[::step]
+def lagrange(data):
     values = []
     for i, _ in reversed(list(enumerate(data))):
         value = 0
-        for j, _ in enumerate(points):
+        for j, _ in enumerate(data):
             tmp = data[j][1]
-            for k, _ in enumerate(points):
+            for k, _ in enumerate(data):
                 if j != k:
-                    tmp *= (data[i][0] - points[k][0]) / (points[j][0] - points[k][0])
+                    tmp *= (data[i][0] - data[k][0]) / (data[j][0] - data[k][0])
             value += tmp
         values.append(value)
     return values
 
 
+def spline(data):
+    # data = [(1, 6), (3, -2), (5, 4)]
+    n = len(data) - 1
+    equations_number = n * 4
+    coefficients_number = 4
+    matrix = np.array([[0 for x in range(equations_number)] for y in range(equations_number)])
+    b = np.array([0 for x in range(equations_number)])
+
+    for j in range(0, n):
+        h = data[j + 1][0] - data[j][0]
+
+        matrix[coefficients_number * j][coefficients_number * j] = 1
+        b[4 * j] = data[j][1]
+
+        for i in range(coefficients_number):
+            matrix[coefficients_number * j + 1][coefficients_number * j + i] = h ** i
+            b[coefficients_number * j + 1] = data[j + 1][1]
+
+        if j > 0:
+            for i in range(coefficients_number):
+                if i == 0:
+                    matrix[coefficients_number * (j - 1) + 2][i + coefficients_number * (j - 1)] = i
+                else:
+                    matrix[coefficients_number * (j - 1) + 2][i + coefficients_number * (j - 1)] = i * h ** (
+                            i - 1)
+            matrix[coefficients_number * (j - 1) + 2][1 + coefficients_number * j] = -1
+
+        if j > 0:
+            matrix[coefficients_number * (j - 1) + 3][coefficients_number * (j - 1) + 2] = 2
+            matrix[coefficients_number * (j - 1) + 3][coefficients_number * (j - 1) + 3] = 6 * h
+            matrix[coefficients_number * (j - 1) + 3][coefficients_number * j + 2] = -2
+
+        if j == n - 2:
+            matrix[equations_number - 2][2] = 2
+        if j == n - 1:
+            matrix[equations_number - 1][-2] = 2
+            matrix[equations_number - 1][-1] = 6 * h
+
+    result = np.linalg.solve(matrix, b)
+    return result
+
+
 if __name__ == '__main__':
     data = pandas.read_csv('WielkiKanionKolorado.csv').values
+
     # steps = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-    values = lagrange(10, data)
+
+    # values = lagrange(data[::10])
+    # font = {'family': 'Arial', 'size': 10}
+    # pyplot.rc('font', **font)
+    # pyplot.plot(range(len(data)), values)
+    # pyplot.xlabel('x')
+    # pyplot.ylabel('y')
+    # pyplot.title('Lagrange interpolation')
+    # pyplot.show()
+
+    values = spline(data[::10])
     font = {'family': 'Arial', 'size': 10}
     pyplot.rc('font', **font)
     pyplot.plot(range(len(data)), values)
-    pyplot.xlabel('Matrix size')
-    pyplot.ylabel('Execution time [s]')
-    pyplot.title('Linear equations calculation methods execution times')
+    pyplot.xlabel('x')
+    pyplot.ylabel('y')
+    pyplot.title('Spline interpolation')
     pyplot.show()
+    test = ""
