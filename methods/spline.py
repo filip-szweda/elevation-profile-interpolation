98409bdb3db37acef58f2da0f09d2fdd7bab6ebc
FilipSzweda <filip.szweda1@gmail.com>
2022-06-06 00:56:19 +0200

    Shit's a bit better

--- methods/spline.py
+++ methods/spline.py
@@ -7,40 +7,39 @@ def spline(original_data, knots_number):
     n = len(data) - 1
     coefficients_number = 4
     equations_number = n * coefficients_number
-    matrix = numpy.array([[0.0 for x in range(equations_number)] for y in range(equations_number)])
-    b = numpy.array([0.0 for x in range(equations_number)])
+    A = numpy.array([[0.0 for _ in range(equations_number)] for _ in range(equations_number)])
+    b = numpy.array([0.0 for _ in range(equations_number)])
 
-    for j in range(n):
-        h = data[j + 1][0] - data[j][0]
+    for i in range(n):
+        h = data[i + 1][0] - data[i][0]
 
-        matrix[coefficients_number * j][coefficients_number * j] = 1
-        b[coefficients_number * j] = data[j][1]
+        A[coefficients_number * i][coefficients_number * i] = 1
+        b[coefficients_number * i] = data[i][1]
 
-        for i in range(coefficients_number):
-            matrix[coefficients_number * j + 1][coefficients_number * j + i] = h ** i
-            b[coefficients_number * j + 1] = data[j + 1][1]
+        for j in range(coefficients_number):
+            A[coefficients_number * i + 1][coefficients_number * i + j] = h ** j
+            b[coefficients_number * i + 1] = data[i + 1][1]
 
-        if j > 0:
-            for i in range(coefficients_number):
-                if i == 0:
-                    matrix[coefficients_number * (j - 1) + 2][i + coefficients_number * (j - 1)] = i
+        if i > 0:
+            for j in range(coefficients_number):
+                if j == 0:
+                    A[coefficients_number * (i - 1) + 2][j + coefficients_number * (i - 1)] = j
                 else:
-                    matrix[coefficients_number * (j - 1) + 2][i + coefficients_number * (j - 1)] = i * h ** (
-                            i - 1)
-            matrix[coefficients_number * (j - 1) + 2][1 + coefficients_number * j] = -1
+                    A[coefficients_number * (i - 1) + 2][j + coefficients_number * (i - 1)] = j * h ** (j - 1)
+            A[coefficients_number * (i - 1) + 2][1 + coefficients_number * i] = -1
 
-        if j > 0:
-            matrix[coefficients_number * (j - 1) + 3][coefficients_number * (j - 1) + 2] = 2
-            matrix[coefficients_number * (j - 1) + 3][coefficients_number * (j - 1) + 3] = 6 * h
-            matrix[coefficients_number * (j - 1) + 3][coefficients_number * j + 2] = -2
+        if i > 0:
+            A[coefficients_number * (i - 1) + 3][coefficients_number * (i - 1) + 2] = 2
+            A[coefficients_number * (i - 1) + 3][coefficients_number * (i - 1) + 3] = 6 * h
+            A[coefficients_number * (i - 1) + 3][coefficients_number * i + 2] = -2
 
-        if j == n - 2:
-            matrix[equations_number - 2][2] = 2
-        if j == n - 1:
-            matrix[equations_number - 1][-2] = 2
-            matrix[equations_number - 1][-1] = 6 * h
+        if i == n - 2:
+            A[equations_number - 2][2] = 2
+        if i == n - 1:
+            A[equations_number - 1][-2] = 2
+            A[equations_number - 1][-1] = 6 * h
 
-    coefficients = numpy.linalg.solve(matrix, b)
+    coefficients = numpy.linalg.solve(A, b)
 
     values = []
     for i in range(len(original_data) - 1):
