5e47158d62277c3af6eec436de2846462ca0c052
FilipSzweda <filip.szweda1@gmail.com>
2022-06-06 13:42:27 +0200

    Fix lagrange

--- methods/lagrange.py
+++ methods/lagrange.py
@@ -2,7 +2,7 @@ def lagrange(original_data, knots_number):
     step = int(round(len(original_data) / knots_number * 2) / 2)
     data = original_data[::step]
     values = []
-    for i, _ in reversed(list(enumerate(original_data))):
+    for i in range(int(data[len(data) - 1][0] + 1)):
         value = 0
         for j, _ in enumerate(data):
             tmp = data[j][1]
